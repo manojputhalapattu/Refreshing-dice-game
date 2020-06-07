3f8b371d6d22b88ff84b9f11d65a70ae0f1f8786
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-06-07 19:03:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var random=Math.floor(Math.random()*6)+1
+//var rand="dice"+random+".png"
+//var source="images\"+rand;
+var name="images/dice"+random+".png"
+document.querySelectorAll("img")[0].setAttribute("src",name);
+var random2=Math.floor(Math.random()*6)+1
+var d1="images/dice"+random2+".png"
+document.querySelectorAll("img")[1].setAttribute("src",d1);
+if(random>random2){
+    document.querySelector("h2").innerHTML="Player 1 wins";
+}
+else if(random2>random){
+    document.querySelector("h2").innerHTML="Player 2 wins";
+}
+if(random==random2){
+document.querySelector("h2").innerHTML="Draw";
+}
